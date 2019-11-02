d79196594a26c7f21d26740ab33bbf62b4f3e197
Christopher Adams <cadams@travelclick.com>
2019-11-02 19:49:22 -0400

    Added basics for React Testing Library

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Counter from "./components/Counter";
+import Todos from "./components/Todos";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("<Counter />", () => {
+    it("properly increments and decrements the counter", () => {
+        const { getByText } = render(<Counter />);
+        const counter = getByText("0");
+        const incrementButton = getByText("+");
+        const decrementButton = getByText("-");
+
+        fireEvent.click(incrementButton);
+        expect(counter.textContent).toEqual("1");
+
+        fireEvent.click(decrementButton);
+        expect(counter.textContent).toEqual("0");
+    });
+});
+
+test("adds a new to-do", () => {
+    const { getByPlaceholderText, getByText } = render(<Todos />);
+    const input = getByPlaceholderText(/add something/i);
+    const todo = "Read Master React Testing";
+
+    getByText("No to-dos!");
+
+    fireEvent.change(input, { target: { value: todo } });
+    fireEvent.keyDown(input, { key: "Enter" });
+
+    getByText(todo);
+    expect(input.value).toBe("");
 });
